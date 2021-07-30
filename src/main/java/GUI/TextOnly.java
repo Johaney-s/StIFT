2b135df2c8e285d9f171d91b6198db59cb2c2d95
Johana Supíková <xsupikov@fi.muni.cz>
2021-07-30 21:30:50 +0200

    Add ZAMS option to CLI-textmode
    * Now it is possible to specify ZAMS phase when using custom grid in ZAMS

--- src/main/java/GUI/TextOnly.java
+++ src/main/java/GUI/TextOnly.java
@@ -4,9 +4,12 @@ package GUI;
 import backend.*;
 import backend.objects.ResultStar;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 /**
  * Alternative class for printing steps to console
@@ -48,23 +51,55 @@ public class TextOnly {
 
         try {
             InputStream is = TextOnly.class.getClassLoader().getResourceAsStream("Data.txt");
-            if (args.length == 6 || args.length == 4) {
+            boolean gridIsCustom = args.length == 6 || args.length == 4;
+            if (gridIsCustom) {
                 is = new FileInputStream(args[args.length - 1]);
                 System.out.println("Grid file: " + args[args.length - 1]);
             } else {
                 System.out.println("Grid file: default (PARSEC + COLIBRI)");
             }
             data = GridFileParser.extract(is);
+
+            // try to set zams for custom grid
+            Short phaseZAMS = null;
+            if (gridIsCustom) {
+                System.out.println("Select ZAMS phase or type [N] to skip: " + data.getCurrentPhases().stream().sorted().collect(Collectors.toList()));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                while (true) {
+                    Short inputZamsPhase;
+                    String zams = reader.readLine();
+                    if (zams.equalsIgnoreCase("N"))
+                        break;
+                    try {
+                        inputZamsPhase = Short.parseShort(zams);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Incorrect phase. Skip [N] or choose one of the phases included in your data file: "
+                                + data.getCurrentPhases().stream().sorted().collect(Collectors.toList()));
+                        continue;
+                    }
+
+                    if (!data.getCurrentPhases().contains(inputZamsPhase)) {
+                        System.out.println("Incorrect phase. Skip [N] or choose one of the phases included in your data file: "
+                                + data.getCurrentPhases().stream().sorted().collect(Collectors.toList()));
+                    } else {
+                        phaseZAMS = inputZamsPhase;
+                        break;
+                    }
+                }
+            }
+
             Settings settings = new Settings();
-            if (args.length == 6 || args.length == 4) {
+            if (!gridIsCustom) {
                 settings.setDefaultSettings();
             } else {
-                settings = new Settings(data.getCurrentPhases(), null, false);
+                settings = new Settings(data.getCurrentPhases(), phaseZAMS, false);
             }
             data.applySettings(settings);
             Data.setCurrentData(data);
 
             System.out.println("Total number of isochrones: " + data.getGroupedData().size());
+            if(settings.getPhaseZams()!=null)
+                System.out.println("ZAMS phase: " + settings.getPhaseZams().toString());
             System.out.printf("Input:%n%.4f\t%.4f uncertainties: %.4f\t%.4f%n", x, y, x_unc, y_unc);
             ComputationStats stats = data.estimateStats(x, y, x_unc, y_unc, rounding);
             System.out.println("Estimation method: " + stats.getResult().getResultType());
